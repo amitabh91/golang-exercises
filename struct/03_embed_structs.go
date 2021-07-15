a2115da0fc541b8f996bae43a1cd844cf556ba8f
Amitabh Sinha <amitabh.sinha@mavenir.com>
2021-07-15 18:22:28 +0530

    embeded struct in go

---
+++ struct/03_embed_structs.go
@@ -0,0 +1,58 @@
+/* Create a struct type vehicle, truck, and sedan with below-mentioned fields. And embed struct truck and sedan with struct type vehicle
+vehicle fields:
+	door
+	color
+truck fields:
+	sixWheel  should be type bool
+sedan fields:
+	luxury should be type bool
+
+Print each fields of struct truck and sedan.
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+type vehicle struct {
+	doors int
+	color string
+}
+
+type truck struct {
+	vehicle
+	sixWheel bool
+}
+
+type sedan struct {
+	vehicle
+	luxury bool
+}
+
+func main() {
+
+	t := truck{
+		sixWheel: true,
+		vehicle: vehicle{
+			doors: 2,
+			color: "Black",
+		},
+	}
+	s := sedan{
+		luxury: true,
+		vehicle: vehicle{
+			doors: 4,
+			color: "red",
+		},
+	}
+	// printing eaching fields values
+	fmt.Println("vechile is type truck:", t.sixWheel)
+	fmt.Println("doors:", t.doors)
+	fmt.Println("color:", t.color)
+	fmt.Println("===========")
+	fmt.Println("vechile is type Sedan:", s.luxury)
+	fmt.Println("doors:", s.doors)
+	fmt.Println("color:", s.color)
+}
